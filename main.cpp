2bec24a53ad21894377bdc917312ebfada96401c
iwWh1te <ylanovru10@gmail.com>
2023-05-23 22:50:01 +0400

    dvizhenie

--- main.cpp
+++ main.cpp
@@ -1,43 +1,85 @@
-#include <QApplication>
-#include <QFont>
-#include <QLabel>
-#include <QPushButton>
-#include <QVBoxLayout>
-#include <QWidget>
+#include <QtWidgets>
 
-int main(int argc, char *argv[]) {
+class MainWindow : public QMainWindow {
+public:
+    MainWindow();
+    void startButtonClick();
+    void stopButtonClick();
+    void runClock();
+protected:
+    void paintEvent(QPaintEvent *event);
+private:
+    QPushButton *startButton;
+    QPushButton *stopButton;
+    QLabel      *textLabel;
+    QTimer      *timer;
+    int         seconds;
+    int         timerInterval;
+    int         xSpot;
+};
+
+MainWindow::MainWindow() {
+    resize(900, 500);
+    setWindowTitle("Main v9");
+    QFont labelFont("Courier", 24, QFont::Bold);
+    startButton = new QPushButton("Start", this);
+    startButton->setGeometry(100, 300, 150, 50);
+    startButton->setFont(QFont("Time", 20, QFont::Bold));
+
+    stopButton = new QPushButton("Stop", this);
+    stopButton->setGeometry(300, 300, 150, 50);
+    stopButton->setFont(QFont("Time", 20, QFont::Bold));
+
+    textLabel = new QLabel (this);
+    textLabel->setGeometry(100, 20, 350, 180);
+    textLabel->setFont(labelFont);
+
+    connect(startButton, &QPushButton::clicked, this, &MainWindow::startButtonClick);
+    connect(stopButton, &QPushButton::clicked, this, &MainWindow::stopButtonClick);
+
+    timer = new QTimer (this);
+    connect (timer, &QTimer::timeout, this, &MainWindow::runClock);
+    seconds = 0;
+    timerInterval = 10;
+
+    xSpot = 0;
+}
+
+void MainWindow::paintEvent(QPaintEvent *) {
+    QPainter painter (this);
+    QPen darkGreenPen(Qt::darkGreen, 4, Qt::SolidLine, Qt::RoundCap);
+    QBrush greenBrush (Qt::green, Qt::SolidPattern);
+    painter.setPen (darkGreenPen);
+    painter.setBrush (greenBrush);
+    painter.drawEllipse(xSpot, 200, 80, 60);
+}
+
+void MainWindow::startButtonClick() {
+    textLabel->setText ("Start1");
+    timer->setInterval (timerInterval);
+    timer->start();
+}
+
+void MainWindow::stopButtonClick() {
+    textLabel->setText ("Stop2");
+    timer->stop();
+    seconds = 0;
+}
+
+void MainWindow::runClock() {
+    QString strSeconds = QString::number (seconds);
+    textLabel->setText (strSeconds);
+    seconds++;
+
+    xSpot++;
+    update();
+}
+
+
+
+int main (int argc, char *argv[]) {
     QApplication app(argc, argv);
-
-    QWidget *window = new QWidget;
-    window->setWindowTitle("Change Font Size");
-
-    QLabel *label = new QLabel("Hello World!");
-    QFont font = label->font();
-    font.setPointSize(20);
-    label->setFont(font);
-
-    QPushButton *plusButton = new QPushButton("+");
-    QPushButton *minusButton = new QPushButton("-");
-
-    QObject::connect(plusButton, &QPushButton::clicked, [&label]() {
-        QFont font = label->font();
-        font.setPointSize(font.pointSize() + 1);
-        label->setFont(font);
-    });
-
-    QObject::connect(minusButton, &QPushButton::clicked, [&label]() {
-        QFont font = label->font();
-        font.setPointSize(font.pointSize() - 1);
-        label->setFont(font);
-    });
-
-    QVBoxLayout *layout = new QVBoxLayout;
-    layout->addWidget(label);
-    layout->addWidget(plusButton);
-    layout->addWidget(minusButton);
-
-    window->setLayout(layout);
-    window->show();
-
+    MainWindow mainWin;
+    mainWin.show();
     return app.exec();
 }
